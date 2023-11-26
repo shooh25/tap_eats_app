dab22760a3cc0f118f31ebd362f489d3722d52be
shooh25 <koan6210@outlook.jp>
2023-11-26 23:47:53 +0900

    style: ローディングアニメーション

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,8 @@
 const Loader: React.FC = () => {
   return (
     <>
-      <div>
-        <p>Loading ... </p>
+      <div className="relative w-full h-full flex align-center justify-center">
+        <div className="animate-spin h-8 w-8 border-2 border-primary rounded-full border-t-transparent"></div>
       </div>
     </>
   )
